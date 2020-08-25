4c00795b563b7413c440036e52cfa311ac3dd0fb
cassiosantanna <cassiojl1407@gmail.com>
2020-08-25 11:51:16 -0300

    Criação do Insert
    Criada a parte de inserção de informações no banco de dados.

--- app.js
+++ app.js
@@ -3,16 +3,35 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const handlebars = require('express-handlebars')
 const app = express()
-
+const urlencodeParser = bodyParser.urlencoded({extended:false})
+const sql = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306
+})
+sql.query("use crud_node")
 
 // Tamplate engine
 app.engine("handlebars", handlebars ({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+app.use('/css', express.static('css'))
+app.use('/js', express.static('js'))
+app.use('/img', express.static('img'))
 
 
 // Routes and tamplate
 app.get('/', function(req, res) {
     res.render('index')
+})
+
+app.get('/inserir', function(req, res) {
+    res.render('inserir')
+})
+
+app.post("/controllerForm", urlencodeParser, function (req, res) {
+    sql.query("insert into usuarios values (?,?,?)", [req.body.id, req.body.nome, req.body.idade])
+    res.render('controllerForm', {name: req.body.nome})
 })
 
 //start server
