dc1093378d433acc618903ebb7bf745a482f36dc
cassiosantanna <cassiojl1407@gmail.com>
2020-08-25 10:26:43 -0300

    Servidor e aplicações
    Criando conexão com servidor.
    Instalando Express, nodemon,  mysql, body-oarser e express-handlebars.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+const handlebars = require('handlebars')
+const app = express()
+
+
+//start server
+app.listen(3000, function(req, res) {
+    console.log('Servidor Rodando')
+})
