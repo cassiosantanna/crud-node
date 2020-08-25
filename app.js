e1371efe2aa48b82c4c2fc37876a8cb836f571e7
cassiosantanna <cassiojl1407@gmail.com>
2020-08-25 10:53:41 -0300

    Criando primeira rota
    Primeira rota criada no node.js chamando a index do arquivo principal main usando o handlebars

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
-const handlebars = require('handlebars')
+const handlebars = require('express-handlebars')
 const app = express()
 
+
+// Tamplate engine
+app.engine("handlebars", handlebars ({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+
+// Routes and tamplate
+app.get('/', function(req, res) {
+    res.render('index')
+})
 
 //start server
 app.listen(3000, function(req, res) {
